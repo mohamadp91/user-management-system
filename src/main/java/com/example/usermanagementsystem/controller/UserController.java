513ae0051315bdef86c3e630642d0fe956aff0e5
mohamadp91 <mohamad7291m@gmail.com>
2021-08-31 19:05:31 +0430

    Integrate resilience4j

--- src/main/java/com/example/usermanagementsystem/controller/UserController.java
+++ src/main/java/com/example/usermanagementsystem/controller/UserController.java
@@ -2,12 +2,11 @@ package com.example.usermanagementsystem.controller;
 
 import com.example.usermanagementsystem.model.UserModel;
 import com.example.usermanagementsystem.repository.UserRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class UserController {
@@ -17,16 +16,29 @@ public class UserController {
 
 
     @GetMapping("/users")
+    @CircuitBreaker(name = "main", fallbackMethod = "getAllUsersFallback")
     public List<UserModel> getAllUsers() {
         return userRepository.findAll();
     }
 
+    public List<UserModel> getAllUsersFallback(Throwable t) {
+        ArrayList<UserModel> userList = new ArrayList<>();
+        userList.add(new UserModel());
+        return userList;
+    }
+
     @GetMapping("/users/{id}")
-    public Optional<UserModel> getUserById(@PathVariable long id) {
-        return userRepository.findById(id);
+    @CircuitBreaker(name = "main", fallbackMethod = "getUserFallback")
+    public UserModel getUserById(@PathVariable long id) {
+        return userRepository.getUserModelById(id);
+    }
+
+    public UserModel getUserFallback(Throwable t) {
+        return new UserModel();
     }
 
     @PostMapping(value = "/users")
+    @CircuitBreaker(name = "main", fallbackMethod = "addUserFallback")
     public UserModel addUser(@RequestBody UserModel user) {
 
         Date date = new Date();
@@ -42,11 +54,20 @@ public class UserController {
         return userModel;
     }
 
+    public UserModel addUserFallback(Throwable t) {
+        return new UserModel();
+    }
+
     @DeleteMapping("/users/{id}")
-    public Optional<UserModel> deleteUser(@PathVariable long id) {
-        Optional<UserModel> userModel = userRepository.findById(id);
+    @CircuitBreaker(name = "main", fallbackMethod = "deleteUserFallback")
+    public UserModel deleteUser(@PathVariable long id) {
+        UserModel userModel = userRepository.getUserModelById(id);
         userRepository.deleteById(id);
         return userModel;
+    }
+
+    public UserModel deleteUserFallback(Throwable t){
+        return new UserModel();
     }
 
 }
